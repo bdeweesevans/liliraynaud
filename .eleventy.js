6e5065296d2b70b79a23f15257836dddcbe73dfb
bdeweesevans <bdeweesevans@gmail.com>
2024-04-01 13:53:40 -0400

    404 2

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,5 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("formstyle.css");
 	eleventyConfig.addPassthroughCopy("assets");
 	eleventyConfig.addPassthroughCopy("favicon.png");
+	eleventyConfig.addPassthroughCopy("404.html");
 };
---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/52fe6f77-a76b-4ff6-8a56-396ae99e3d6c/deploy-status)](https://app.netlify.com/sites/liliraynaud/deploys)
