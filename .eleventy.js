d38eef8e0183919b583078794f57105be481aa00
bdeweesevans <bdeweesevans@gmail.com>
2024-04-01 11:50:48 -0400

    new deployment workflow

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("style.css");
 	eleventyConfig.addPassthroughCopy("formstyle.css");
 	eleventyConfig.addPassthroughCopy("assets");
-	eleventyConfig.addPassthroughCopy("favicon.ico");
 	eleventyConfig.addPassthroughCopy("favicon.png");
+
+	return {
+        pathPrefix: "/liliraynaud/"	/* need to delete this once its on her own domain */
+    };
 };
